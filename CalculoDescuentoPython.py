37d3fa64717587af93eeb83bf2a00dc18d90fe8a
Mauriciouea <mx.sisalimag@uea.edu.ec>
2025-03-20 04:56:38 -0500

    actualizar

--- CalculoDescuentoPython.py
+++ CalculoDescuentoPython.py
@@ -4,6 +4,17 @@ def calcular_descuento (monto_total, porcentaje_descuento=10):
     #devolver el monto de descuento
     return descuento
 
-monto = 89
-descuento_calculado = calcular_descuento(monto)
-print (f"El monto del descuento es: {descuento_calculado}: el monto a cancelar es {monto}")
+# Programa principal
+if __name__ == "__main__":
+    # Primera llamada a la función con solo el monto total
+    monto1 = 1000  # Monto total de la compra
+    descuento1 = calcular_descuento(monto1)
+    monto_final1 = monto1 - descuento1
+    print(f"Compra 1: Monto total: {monto1}, Descuento: {descuento1}, Monto final a pagar: {monto_final1}")
+
+    # Segunda llamada a la función con monto total y porcentaje de descuento
+    monto2 = 500  # Monto total de la compra
+    porcentaje_descuento2 = 15  # Porcentaje de descuento
+    descuento2 = calcular_descuento(monto2, porcentaje_descuento2)
+    monto_final2 = monto2 - descuento2
+    print(f"Compra 2: Monto total: {monto2}, Descuento: {descuento2}, Monto final a pagar: {monto_final2}")
